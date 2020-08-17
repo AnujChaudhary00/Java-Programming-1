84f1deb2d11822db066933fa011e401d558b103f
Anuj Chaudhary <49245690+AnujChaudhary00@users.noreply.github.com>
2020-08-17 20:42:41 +0530

    update

--- Question6.java
+++ Question6.java
@@ -1,51 +1,36 @@
 import java.util.*;
 
 class Question6 {
-     static String Name;
-    static int year;
-    static String Address;
+      String Name;
+     int year;
+     String Address;
 
-    public static void print() {
+    public void print() {
         System.out.print(Name +"           " + year + "                " + Address);
         System.out.println();
     }
 
     public static void main(String[] args) {
         // first object
+    	Scanner sc = new Scanner(System.in);
+    	
+    	ArrayList<Question6> details=new ArrayList<Question6>();
+    	System.out.println("Enter the no. of details you want to enter");
+    	int n=sc.nextInt();
+    	sc.nextLine();
+    	for(int i=0;i<n;i++)
+    	{
         Question6 emp1 = new Question6();
-        Question6 emp2 = new Question6();
-        Question6 emp3 = new Question6();
-
-        Scanner sc = new Scanner(System.in);
-
-  
         System.out.println("Name: ");
         emp1.Name = sc.nextLine();
         System.out.println("Address: ");
         emp1.Address = sc.nextLine();
         System.out.println("Year: ");
         emp1.year = Integer.parseInt(sc.nextLine());
-
-  
-        System.out.println("Name: ");
-        emp2.Name = sc.nextLine();
-        System.out.println("Address: ");
-        emp2.Address = sc.nextLine();
-        System.out.println("Year: ");
-        emp2.year = Integer.parseInt(sc.nextLine());
-
-
-        System.out.println("Name: ");
-        emp3.Name = sc.nextLine();
-        System.out.println("Address: ");
-        emp3.Address = sc.nextLine();
-        System.out.println("Year: ");
-        emp3.year = Integer.parseInt(sc.nextLine());
-
-        System.out.println("Name:    " + "Year of Joining:  " + "             Address          ");
-        emp1.print();
-        emp2.print();
-        emp3.print();
+        details.add(emp1);
+    	}
+    	 System.out.println("Name:    " + "Year of Joining:  " + "             Address          ");
+    	details.stream().map(x->x).forEach(s->s.print());
     }
 
 }
